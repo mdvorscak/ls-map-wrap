63f569c6195235663657ce468695e6364d77105a
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-06 19:52:48 -0600

    Adding content to tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,27 +1,62 @@
-import { assert } from 'chai';
-//import lsMapWrap from '../../src/index';
+import { expect }  from 'chai';
+import lsMapWrap from '../../src/index';
 import theoretically from 'jasmine-theories';
+
 
 describe('ls-map-wrap', () => {
   describe('has', ()=> {
-    it('should return false when nothing is passed');
-    it('should return false when the element is not in the map');
-    it('should return true when the element is in the map');
+    it('should return false when nothing is passed', () => {
+      expect(lsMapWrap.has()).to.equal(false);
+    });
+    it('should return false when the element is not in the map', () => {
+      expect(lsMapWrap.has()).to.equal(false);
+    });
+    it('should return true when the element is in the map', () => {
+      lsMapWrap.set('test', 'test');
+      expect(lsMapWrap.has('test')).to.equal(true);
+    });
   });
   describe('get/set', ()=> {
-    it('should return undefined when the element is not in the map');
-    theoretically.it('should return the proper element', [0, undefined, null, false, true, 'test', 
+    it('should return undefined when the element is not in the map', () => {
+      let value = lsMapWrap.get('non-existant-key');
+      expect(value).to.equal(undefined);
+    });
+    let testCount = 0;
+    theoretically.it('should return the proper element (%s)', [0, undefined, null, false, true, 'test', 
                                                           Date.now(), [1, 2, 3], Infinity, NaN, () => {},
                                                         {test: 'x'}, Promise.resolve(5)], (element) => {
-      
+      testCount++;
+      let key = `test-${testCount}`;
+      lsMapWrap.set(key, element);
+      let value = lsMapWrap.get(key);
+      expect(value).to.deep.equal(element);
     });
   });
   describe('delete', ()=> {
-    it('should not throw an error when nothing is passed');
-    it('should not throw an error when value that isn\'t in the map is passed');
-    it('should remove an existing item');
+    it('should not throw an error when nothing is passed', () => {
+      expect(() => lsMapWrap.delete()).not.to.throw();
+    });
+    it('should not throw an error when value that isn\'t in the map is passed', () => {
+      expect(() => lsMapWrap.delete('non-existant-key')).not.to.throw();
+    });
+    it('should remove an existing item', () => {
+      let key = 'removingKey';
+      let element = 'test';
+      lsMapWrap.set(key, element);
+      lsMapWrap.delete(key);
+      expect(lsMapWrap.has(key)).to.equal(false);
+    });
   });
   describe('clear', ()=> {
-    it('should remove all items');
+    it('should remove all items', () => {
+      let keys = [1, 2, 3];
+      keys.forEach(element => {
+        lsMapWrap.set(element, element);
+      });
+      lsMapWrap.clear();
+      keys.forEach(element => {
+        expect(lsMapWrap.has(element)).to.equal(false);
+      });
+    });
   });
 });
