63a2e1fe7a19512c593e013731450509fb09bbbc
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-06 19:08:12 -0600

    Adding Karma + replacing babel with webpack

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+var webpackConfig = require('./webpack.config.js');
+
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    preprocessors: {
+      'src/index.js': ['webpack'],
+      'test/**/*.js': ['webpack']
+    },
+    files: ['src/index.js', 'test/**/*.js'],
+    reporters: ['progress'],
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true
+    },
+    port: 9876,  // karma web server port
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
+    singleRun: true, // Karma captures browsers, runs the tests and exits
+    concurrency: Infinity
+  })
+}
