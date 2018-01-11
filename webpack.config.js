c6ba0edffdd4bf9dbdc0b61966da687b0702a43a
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-01-10 22:23:33 -0600

    Fix minification process

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
-const path = require('path');
+var webpack = require('webpack');
+var path = require('path');
+var entry = 'index';
+var outputFile = entry + '.js';
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
+var config = {
+  entry: __dirname + '/src/index.js',
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'lib')
-  }
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'lsMap',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [require.resolve('babel-preset-env')]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new UglifyJSPlugin()]
 };
+
+module.exports = config;
